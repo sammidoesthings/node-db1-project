47ecbf51d53fcaf9d41f4c383b910977b42cb94f
Samantha <samanthaltl.sfh@gmail.com>
2022-01-26 12:08:37 -0800

    endpoints and router

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,55 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  try {
+  //add something
+    res.json('get accounts')
+  } catch (err) {
+    next(err)
+  }
+  })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  try {
+  //add something
+    res.json('get account by id')
+  } catch (err) {
+    next(err)
+  }
+  })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  try {
+  //add something
+    res.json('post new account')
+
+  } catch (err) {
+    next(err)
+  }
+  })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-});
+  try {
+  //add something
+    res.json('update account')
+
+  } catch (err) {
+    next(err)
+  }
+  });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  try {
+  //add something
+    res.json('delete account')
+
+  } catch (err) {
+    next(err)
+  }
+  })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+  res.status(err.status || 500).json({ message: err.message })
+  })
 
 module.exports = router;
